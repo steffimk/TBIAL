ba16c118862b2ceedff59051e6b486b94009ab2e
Sophia Münch <muench@cip.ifi.lmu.de>
2021-04-30 19:59:36 +0200

    basis for game table started

---
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameTable.java
@@ -0,0 +1,15 @@
+package de.lmu.ifi.sosy.tbial;
+
+
+/**
+ * Game Table
+ */
+@AuthenticationRequired
+public class GameTable extends BasePage {
+
+  /** UID for serialization. */
+  private static final long serialVersionUID = 1L;
+
+  public GameTable() {}
+
+}
