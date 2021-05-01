e3c27cbf0a947e5b29059a2c582bf3df2a990e5c
Steffi <stefanie.m.kloss@t-online.de>
2021-05-01 18:36:28 +0200

    Basics: Added New-Game-Button (Mockup) to Lobby

--- de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/Lobby.java
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/Lobby.java
@@ -2,13 +2,19 @@ package de.lmu.ifi.sosy.tbial;
 
 import de.lmu.ifi.sosy.tbial.db.User;
 import java.util.List;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.util.time.Duration;
 
 /**
@@ -23,6 +29,8 @@ public class Lobby extends BasePage {
   /** UID for serialization. */
   private static final long serialVersionUID = 1L;
 
+  private final AjaxLink newGameButton;
+
   public Lobby() {
     IModel<List<User>> playerModel = (IModel<List<User>>) () -> getTbialApplication().getLoggedInUsers();
 
@@ -36,12 +44,30 @@ public class Lobby extends BasePage {
       }
     };
 
+    newGameButton =
+        new AjaxLink<Void>("newGameButton") {
+
+          /** UID for serialization. */
+          private static final long serialVersionUID = 1;
+
+          @Override
+          public void onClick(AjaxRequestTarget target) {
+            createNewGame("defaultName", 4, "pw"); // TODO SK
+          }
+        };
+
     WebMarkupContainer playerListContainer = new WebMarkupContainer("playerlistContainer");
     playerListContainer.add(playerList);
     playerListContainer.add(new AjaxSelfUpdatingTimerBehavior(Duration.seconds(10)));
     playerListContainer.setOutputMarkupId(true);
 
     add(playerListContainer);
+    add(newGameButton);
   }
 
+  /** */
+  private void createNewGame(String name, int maxPlayers, String password) {
+    // TODO SK
+    System.out.println("Create new game called");
+  }
 }
