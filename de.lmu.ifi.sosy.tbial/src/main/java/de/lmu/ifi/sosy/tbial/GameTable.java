884359c9fae12b4dcc9230491420703db327d99c
Sophia Münch <muench@cip.ifi.lmu.de>
2021-05-05 19:20:05 +0200

    game table view added to basepage; chat added to sidebar for now

--- de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameTable.java
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameTable.java
@@ -1,15 +1,20 @@
 package de.lmu.ifi.sosy.tbial;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 
-/**
- * Game Table
- */
+/** Game Table */
 @AuthenticationRequired
 public class GameTable extends BasePage {
 
   /** UID for serialization. */
   private static final long serialVersionUID = 1L;
 
-  public GameTable() {}
-
+  public GameTable() {
+    int players = 4;
+    WebMarkupContainer container = new WebMarkupContainer("myContainer");
+    add(container);
+    if (players != 4) {
+      container.setVisible(false);
+    }
+  }
 }
--- de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/TBIALApplication.java
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/TBIALApplication.java
@@ -84,6 +84,7 @@ public class TBIALApplication extends WebApplication {
     mountPage("login", Login.class);
     mountPage("register", Register.class);
     mountPage("lobby", Lobby.class);
+    mountPage("gameTable", GameTable.class);
   }
 
   /**
