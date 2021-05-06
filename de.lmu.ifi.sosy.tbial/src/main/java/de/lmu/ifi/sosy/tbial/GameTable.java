fa7545e924df4c97b3aa8383cd5055d9a7572d90
Sophia Münch <muench@cip.ifi.lmu.de>
2021-05-06 13:08:09 +0200

    game table layout for 5, 6, 7 players added

--- de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameTable.java
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameTable.java
@@ -10,11 +10,30 @@ public class GameTable extends BasePage {
   private static final long serialVersionUID = 1L;
 
   public GameTable() {
-    int players = 4;
-    WebMarkupContainer container = new WebMarkupContainer("myContainer");
-    add(container);
-    if (players != 4) {
-      container.setVisible(false);
+    int players = 7;
+    WebMarkupContainer container4 = new WebMarkupContainer("myContainer4");
+    WebMarkupContainer container5 = new WebMarkupContainer("myContainer5");
+    WebMarkupContainer container6 = new WebMarkupContainer("myContainer6");
+    WebMarkupContainer container7 = new WebMarkupContainer("myContainer7");
+    add(container4);
+    add(container5);
+    add(container6);
+    add(container7);
+    container4.setVisible(false);
+    container5.setVisible(false);
+    container6.setVisible(false);
+    container7.setVisible(false);
+    if (players == 4) {
+        container4.setVisible(true);
+      }
+    if (players == 5) {
+      container5.setVisible(true);
+    }
+    if (players == 6) {
+        container6.setVisible(true);
+    }
+    if (players == 7) {
+        container7.setVisible(true);
     }
   }
 }
