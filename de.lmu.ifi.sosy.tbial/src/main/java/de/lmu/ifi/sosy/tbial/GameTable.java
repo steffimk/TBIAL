126205c4de67d2ad8666274f99bb76329c58344b
puehl <puehl@cip.ifi.lmu.de>
2021-05-07 12:07:21 +0200

    tried to fill player area using wicket panels, not working yet

--- de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameTable.java
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameTable.java
@@ -1,6 +1,8 @@
 package de.lmu.ifi.sosy.tbial;
 
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.Component;
 
 /** Game Table */
 @AuthenticationRequired
@@ -8,11 +10,18 @@ public class GameTable extends BasePage {
 
   /** UID for serialization. */
   private static final long serialVersionUID = 1L;
+  
+  //private Component playerAreaPanel;
+  //private Fragment fragment;
 
   public GameTable() {
     int players = 4;
     WebMarkupContainer container = new WebMarkupContainer("myContainer");
     add(container);
+    
+    //add(playerAreaPanel = new PlayerAreaPanel("playerAreaPanel"));
+    //add(fragment = new Fragment("myContainer", "playerArea", this));
+    
     if (players != 4) {
       container.setVisible(false);
     }
---
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/PlayerAreaPanel.java
@@ -0,0 +1,14 @@
+package de.lmu.ifi.sosy.tbial;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class PlayerAreaPanel extends Panel{
+
+	/** UID for serialization. */
+	private static final long serialVersionUID = 1L;
+
+	public PlayerAreaPanel(String id) {
+		super(id);
+	}
+
+}
