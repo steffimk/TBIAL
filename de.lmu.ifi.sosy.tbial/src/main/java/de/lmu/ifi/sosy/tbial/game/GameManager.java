7407601668781e7819e0bdf11d38663deaf350d1
Steffi <stefanie.m.kloss@t-online.de>
2021-05-13 18:16:15 +0200

    Test create new game and if game is added to current games in GameManager

--- de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/game/GameManager.java
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/game/GameManager.java
@@ -1,5 +1,6 @@
 package de.lmu.ifi.sosy.tbial.game;
 
+import static java.util.Objects.requireNonNull;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,12 +19,14 @@ public class GameManager {
   }
 
   public synchronized void addGame(Game game) {
+    requireNonNull(game);
     if (!gameNameTaken(game.getName())) {
 	    this.currentGames.put(game.getName(), game);
 	}
   }
 
   public boolean gameNameTaken(String name) {
+    requireNonNull(name);
     for (String gameName : currentGames.keySet()) {
       if (gameName.equals(name)) {
         return true;
