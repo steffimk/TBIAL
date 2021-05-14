5a9c6a53ee07aabba637742871738d99edb8e639
Steffi <stefanie.m.kloss@t-online.de>
2021-05-15 00:34:57 +0200

    Added info whether player is host, improved design of startGameLink

--- de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameLobby.java
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameLobby.java
@@ -2,8 +2,15 @@ package de.lmu.ifi.sosy.tbial;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 import de.lmu.ifi.sosy.tbial.db.User;
 import de.lmu.ifi.sosy.tbial.game.Game;
@@ -18,25 +25,33 @@ public class GameLobby extends BasePage {
   
   private static final Logger LOGGER = LogManager.getLogger(GameLobby.class);
 
-  private final Button startGameButton;
+  private final Link<Void> startGameLink;
+  private final Label isHostLabel;
 
   public GameLobby() {
-    startGameButton =
-        new Button("startGameButton") {
 
-          /** UID for serialization. */
-          private static final long serialVersionUID = 1;
+    TBIALSession session = getSession();
+    String hostName = session.getCurrentGame().getHost();
+    Model<String> isHostModel =
+        session.getUser().getName().equals(hostName)
+            ? new Model<String>("You are the host of this game.")
+            : new Model<String>(hostName + " is the host of this game.");
+    isHostLabel = new Label("isHostLabel", isHostModel);
+
+    startGameLink =
+        new Link<Void>("startGameLink") {
+          private static final long serialVersionUID = 1L;
 
           @Override
-          public void onSubmit() {
+          public void onClick() {
             startGame();
           }
         };
+    startGameLink.setOutputMarkupId(true);
+    startGameLink.setVisible(session.getUser().getName().equals(hostName));
 
-    Form<?> startGameForm = new Form<>("startGameForm");
-    startGameForm.add(startGameButton);
-    
-    add(startGameForm);
+    add(isHostLabel);
+    add(startGameLink);
   }
 
   /**
