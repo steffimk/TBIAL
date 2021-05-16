620ea233382735af4e6c135689d2aaa4703fec42
Lili <l.gainan@campus.lmu.de>
2021-05-16 11:09:31 +0200

    Added game lobby chat

---
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/ChatMessage.java
@@ -0,0 +1,24 @@
+package de.lmu.ifi.sosy.tbial;
+
+import java.io.Serializable;
+
+public class ChatMessage implements Serializable {
+ private static final long serialVersionUID = 1L;
+   
+ private String sender;
+ private String textMessage;
+   
+ public ChatMessage(String sender, String textMessage) {
+  super();
+  this.sender = sender;
+  this.textMessage = textMessage;
+ }
+  
+ public String getSender() {
+    return sender;
+  }
+
+  public String getTextMessage() {
+    return textMessage;
+  }
+}
--- de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameLobby.java
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/GameLobby.java
@@ -1,8 +1,92 @@
 package de.lmu.ifi.sosy.tbial;
 
+import java.util.LinkedList;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.RestartResponseAtInterceptPageException;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.time.Duration;
+
 @AuthenticationRequired
 public class GameLobby extends BasePage {
 
   private static final long serialVersionUID = 1L;
 
+  private static final int maxMessages = 80;
+  private static final LinkedList<ChatMessage> chatMessages = new LinkedList<ChatMessage>();
+  private MarkupContainer chatMessagesContainer;
+  
+  public GameLobby() {
+	  final TextField<String> textField = new TextField<String>("message", new Model<String>());
+	  textField.setOutputMarkupId(true);
+
+    //
+    chatMessagesContainer = new WebMarkupContainer("chatMessages");
+
+    final ListView<ChatMessage> listView =
+        new ListView<ChatMessage>("messages", chatMessages) {
+          private static final long serialVersionUID = 1L;
+
+          @Override
+          protected void populateItem(ListItem<ChatMessage> item) {
+            this.modelChanging();
+
+            ChatMessage chatMessage = item.getModelObject();
+
+            Label sender = new Label("sender", new PropertyModel<String>(chatMessage, "sender"));
+            item.add(sender);
+
+            Label text =
+                new Label("textMessage", new PropertyModel<String>(chatMessage, "textMessage"));
+            item.add(text);
+          }
+        };
+
+    chatMessagesContainer.setOutputMarkupId(true);
+    chatMessagesContainer.add(listView);
+
+    AjaxSelfUpdatingTimerBehavior ajaxBehavior =
+        new AjaxSelfUpdatingTimerBehavior(Duration.seconds(3));
+    chatMessagesContainer.add(ajaxBehavior);
+    add(chatMessagesContainer);
+
+    //
+    Component send =
+        new AjaxSubmitLink("send") {
+          private static final long serialVersionUID = 1L;
+
+          @Override
+          protected void onSubmit(AjaxRequestTarget target) {
+            String username = ((TBIALSession) getSession()).getUser().getName();
+            String text = textField.getModelObject();
+
+            ChatMessage chatMessage = new ChatMessage(username, text);
+
+            synchronized (chatMessages) {
+              if (chatMessages.size() >= maxMessages) {
+                chatMessages.removeLast();
+              }
+
+              chatMessages.addFirst(chatMessage);
+            }
+
+            textField.setModelObject("");
+            target.add(chatMessagesContainer, textField);
+          }
+        };
+
+    Component chatForm = new Form<String>("form").add(textField, send);
+    add(chatForm);
+  }
 }
